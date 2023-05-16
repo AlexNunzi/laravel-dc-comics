e973a4585ba8f7b210a9fdab52669b1afe993b63
Alex Nunzi <alex.nunzi@hotmail.it>
2023-05-16 16:20:06 +0200

    Creato model Comic. Creato resource controller ComicController. Creato controller PageController. Creata struttura base layout con partials header e footer.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Comics')
+
+@section('content')
+
+    <div class="container">
+        <h1 class="text-center">Benvenuto sul sito di Comics-DB</h1>
+    </div>
+
+@endsection

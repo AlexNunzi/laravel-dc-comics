83b2f1e8579159f77bee59082ba7eefdc9d44b9f
Alex Nunzi <alex.nunzi@hotmail.it>
2023-05-17 16:35:54 +0200

    Bonus: Tramite modale di bootstrap aggiunta richiesta di conferma per eliminare gli elementi dalla tabella

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -29,11 +29,38 @@
                                 class="btn btn-primary mx-1">Info</a>
                             <a href="{{ route('comics.edit', ['comic' => $comic->id]) }}"
                                 class="btn btn-secondary mx-1">Modifica</a>
-                            <form method="POST" action="{{ route('comics.destroy', ['comic' => $comic->id]) }}">
-                                @csrf
-                                @method('DELETE')
-                                <button type="sublit" class="btn btn-danger mx-1">Elimina</button>
-                            </form>
+
+                            <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                data-bs-target="#modal{{ $comic->id }}">
+                                Elimina
+                            </button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="modal{{ $comic->id }}" tabindex="-1"
+                                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Attenzione!</h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            Sei sicuro di voler eliminare definitivamente il fumetto dalla tabella?
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary"
+                                                data-bs-dismiss="modal">Annulla</button>
+                                            <form method="POST"
+                                                action="{{ route('comics.destroy', ['comic' => $comic->id]) }}">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger mx-1">Elimina</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </td>
                     </tr>
                 @empty

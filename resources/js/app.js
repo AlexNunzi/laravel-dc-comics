316dc8f15b3d309f8153c976af1c5a4a9a4c4fab
Alex Nunzi <alex.nunzi@hotmail.it>
2023-05-18 18:00:15 +0200

    Modificata logica di comparsa della modal di Bootstrap per poter utilizzare una singola modale per tutti gli elementi della tabella.

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,25 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+
+const deleteButtons = document.querySelectorAll('.form_delete');
+
+deleteButtons.forEach(button => {
+    button.addEventListener('click', event => {
+        event.preventDefault();
+        const formId = event.target.form.id;
+
+        const deleteModal = document.getElementById('confirmDeleteModal');
+
+        const bootstrapModal = new bootstrap.Modal('#confirmDeleteModal');
+        bootstrapModal.show();
+
+        const confirmDeleteButton = deleteModal.querySelector('.confirmDelete');
+
+        confirmDeleteButton.addEventListener('click', () => {
+            const submitForm = document.getElementById(formId);
+            submitForm.submit();
+        });
+    })
+});
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -30,38 +30,13 @@
                             <a href="{{ route('comics.edit', ['comic' => $comic->id]) }}"
                                 class="btn btn-secondary mx-1">Modifica</a>
 
-                            <button type="button" class="btn btn-danger" data-bs-toggle="modal"
-                                data-bs-target="#modal{{ $comic->id }}">
-                                Elimina
-                            </button>
+                            <form id="form{{ $comic->id }}" method="POST"
+                                action="{{ route('comics.destroy', ['comic' => $comic->id]) }}">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="form_delete btn btn-danger mx-1">Elimina</button>
+                            </form>
 
-                            <!-- Modal -->
-                            <div class="modal fade" id="modal{{ $comic->id }}" data-bs-backdrop="static" tabindex="-1"
-                                aria-labelledby="Modal{{ $comic->id }}Label" aria-hidden="true">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h1 class="modal-title fs-5" id="Modal{{ $comic->id }}Label">Attenzione!
-                                            </h1>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                aria-label="Close"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                            Sei sicuro di voler eliminare definitivamente il fumetto dalla tabella?
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary"
-                                                data-bs-dismiss="modal">Annulla</button>
-                                            <form method="POST"
-                                                action="{{ route('comics.destroy', ['comic' => $comic->id]) }}">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger mx-1">Elimina</button>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
                         </td>
                     </tr>
                 @empty
@@ -71,6 +46,28 @@
                 @endforelse
             </tbody>
         </table>
+
+        <!-- Modal -->
+        <div class="modal fade" id="confirmDeleteModal" data-bs-backdrop="static" tabindex="-1"
+            aria-labelledby="deleteModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="deleteModalLabel">Attenzione!
+                        </h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Sei sicuro di voler eliminare definitivamente il fumetto dalla tabella?
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                        <button type="button" class="confirmDelete btn btn-danger">Elimina</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <a href="{{ route('comics.create') }}" class="btn btn-primary">Aggiungi un nuovo fumetto</a>
     </div>
 
